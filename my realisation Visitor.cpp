a6be6290041b5992207f682a45ea71b0cfe6aac6
DerLed <48630545+DerLed@users.noreply.github.com>
2021-04-27 23:19:19 +0300

    Update my realisation Visitor.cpp

--- my realisation Visitor.cpp
+++ my realisation Visitor.cpp
@@ -4,14 +4,14 @@ struct Number;
 struct BinaryOperation;
 
 struct Visitor {
-    virtual void visitNumber(Number const * number) = 0;
-    virtual void visitBinaryOperation(BinaryOperation const * operation) = 0;
+    virtual void visitNumber(Number const* number) = 0;
+    //virtual void visitBinaryOperation(BinaryOperation const* operation) = 0;
     virtual ~Visitor() { }
 };
 
 struct Expression {
     virtual double evaluate() const = 0;
-    virtual void visit(Visitor * vistitor) const = 0;
+    virtual void visit(Visitor* vistitor) const = 0;
     virtual ~Expression() { }
 };
 
@@ -23,22 +23,22 @@ struct Number : Expression {
 
     double get_value() const { return value; }
 
-    void visit(Visitor * visitor) const { visitor->visitNumber(this); }
-    
+    void visit(Visitor* visitor) const { visitor->visitNumber(this); }
+
     ~Number() {}
 private:
     double value;
 };
 
-struct BinaryOperation : Expression{
-    BinaryOperation(Expression const * left, char op, Expression const * right)
+struct BinaryOperation : Expression {
+    BinaryOperation(Expression const* left, char op, Expression const* right)
         : left(left), op(op), right(right)
     { }
     ~BinaryOperation() {
         delete left;
         delete right;
     }
-    
+
     double evaluate() const {
         if (op == '+') return left->evaluate() + right->evaluate();
         if (op == '-') return left->evaluate() - right->evaluate();
@@ -46,41 +46,41 @@ struct BinaryOperation : Expression{
         if (op == '/') return left->evaluate() / right->evaluate();
     }
 
-    Expression const * get_left() const { return left; }
-    Expression const * get_right() const { return right; }
+    Expression const* get_left() const { return left; }
+    Expression const* get_right() const { return right; }
     char get_op() const { return op; }
 
-    void visit(Visitor * visitor) const { visitor->visitBinaryOperation(this); }
+   // void visit(Visitor* visitor) const { visitor->visitBinaryOperation(this); }
 
 private:
-    Expression const * left;
-    Expression const * right;
+    Expression const* left;
+    Expression const* right;
     char op;
 };
 
 
 // Этот класс вам нужно реализовать 
 struct PrintVisitor : Visitor {
-    void visitNumber(Number const * number)
+    void visitNumber(Number const* number)
     {
-        std::cout << number->get_value()  << std::endl;
+        std::cout << number->get_value() << std::endl;
     }
 
-   /* void visitBinaryOperation(BinaryOperation const * bop)
-    {
-        
-    }*/
+    /* void visitBinaryOperation(BinaryOperation const * bop)
+     {
+
+     }*/
 };
 
 
 int main()
 {
-    std::cout<<"Hello World" << std::endl;
+    std::cout << "Hello World" << std::endl;
     Number b(3.5);
-    Expression * a1 = new Number(5.2);
-    std::cout<< b.evaluate() << std::endl;
-    Visitor * a;
-    a->visitNumber(b);
-  //Expression * sube = new BinaryOperation(new Number(4.5), '*', new Number(5));
+    Expression* a1 = new Number(5.2);
+    std::cout << b.evaluate() << std::endl;
+    PrintVisitor * PV;
+    b.visit(PV);
+    //Expression * sube = new BinaryOperation(new Number(4.5), '*', new Number(5));
     return 0;
 }
